5e5b00a04699777eb85879cfca89fed7598f784b
M E Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2024-04-26 23:02:14 -0700

    Relational problem 2

---
+++ lab2.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<set>
+using namespace std;
+int main(){
+
+    int sA,sB;
+    set<int> A,B;
+    cout<<"Enter size of A: ";
+    cin>>sA;
+    cout<<"Enter size of B: ";
+    cin>>sB;
+    
+    cout<<"Enter elements of A:\n";
+    for(int i=1;i<=sA;i++){
+        int eleA;
+        cin>>eleA;
+        A.insert(eleA);
+    }
+
+    cout<<"Enter elements of B:\n";
+    for(int i=1;i<=sB;i++){
+        int eleB;
+        cin>>eleB;
+        B.insert(eleB);
+    }
+
+      //R{(a,b)|a>b}
+    cout<<"Relation:\n";
+    for(int a:A){
+        for(int b:B){
+            cout<< (a>b?1:0)<<" ";
+        }
+        cout<<endl;
+    }
+  return 0;  
+}
