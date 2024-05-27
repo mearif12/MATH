a831488b1fc355e4980ca7bb34f263e64f55811a
M E Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2024-05-27 10:57:27 -0700

    warshall's shortest path algorithm

---
+++ lab10.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+#define INF 999
+int main() {
+    int nV;
+    cout << "Enter number of vertices: ";
+    cin >> nV;
+       
+//creating and printing adjacency matrix
+    int graph[nV][nV];
+    cout << "Enter the adjacency matrix (use " << INF << " for infinity):\n";
+    for (int i = 0; i < nV; i++) {
+        for (int j = 0; j < nV; j++) {
+            cin >> graph[i][j];
+        }
+    }
+    cout<<"the adjacency matrix:"<<endl;
+    for (int i = 0; i < nV; i++) {
+        for (int j = 0; j < nV; j++) {
+            cout << graph[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+//calculating shortest path according to Warshalls algorithm
+    for (int k = 0; k < nV; k++) {
+        for (int i = 0; i < nV; i++) {
+            for (int j = 0; j < nV; j++) {
+                if (graph[i][k] + graph[k][j] < graph[i][j])
+                    graph[i][j] = graph[i][k] + graph[k][j];
+            }
+        }
+    }
+
+//printing the resulting shortest path matrix
+    cout << "Shortest distance between every pair of vertices:\n";
+    for (int i = 0; i < nV; i++) {
+        for (int j = 0; j < nV; j++) {
+            if (graph[i][j] == INF)
+                cout << "INF\t";
+            else
+                cout << graph[i][j] << "\t";
+        }
+        cout << endl;
+    }
+  return 0;
+}
